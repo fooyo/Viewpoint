d5f74a97906526046cf24a6f07c35007c2df70f1
Rick QING <q@fooyo.sg>
2018-01-30 13:17:19 +0800

    decreased keep alive timeout

--- lib/ews/connection.rb
+++ lib/ews/connection.rb
@@ -50,7 +50,7 @@ class Viewpoint::EWS::Connection
     @httpcli.ssl_config.verify_mode = opts[:ssl_verify_mode] if opts[:ssl_verify_mode]
     @httpcli.ssl_config.ssl_version = opts[:ssl_version] if opts[:ssl_version]
     # Up the keep-alive so we don't have to do the NTLM dance as often.
-    @httpcli.keep_alive_timeout = 60
+    @httpcli.keep_alive_timeout = 20
     @httpcli.receive_timeout = opts[:receive_timeout] if opts[:receive_timeout]
     @httpcli.connect_timeout = opts[:connect_timeout] if opts[:connect_timeout]
     @endpoint = endpoint
